836ba896fe746dff190c70e696b73cd885cb1573
LttlMario <bmariomihail@gmail.com>
2026-08-01 00:29:53 +0300

    Enhance Discord config with organization and secondary setup
    Added organization details and secondary Discord configuration.

--- js/discord-config.js
+++ js/discord-config.js
@@ -1,6 +1,53 @@
 // Configurație publică Discord OAuth.
-// Pentru mutarea pe altă aplicație Discord, modifică doar valorile din acest fișier.
+// Pentru mutarea pe altă aplicație Discord, modifică doar valorile de mai jos.
+
 window.PANEL_DISCORD_CONFIG = Object.freeze({
-    clientId: '1531023771211792384',
-    scopes: ['identify', 'email', 'guilds.members.read']
+
+    organization: {
+        id: "familia-es-todo",
+        name: "Familia Es Todo"
+    },
+
+    // Discord principal folosit la autentificare
+    clientId: "1531023771211792384",
+
+    scopes: [
+        "identify",
+        "email",
+        "guilds.members.read"
+    ],
+
+    // Discord-uri configurate pentru organizație
+    discords: [
+
+        {
+            id: "principal",
+            name: "Discord Principal",
+            enabled: true,
+
+            clientId: "1531023771211792384",
+
+            scopes: [
+                "identify",
+                "email",
+                "guilds.members.read"
+            ]
+        },
+
+        {
+            id: "secondary",
+            name: "Discord Secundar",
+            enabled: false,
+
+            clientId: "",
+
+            scopes: [
+                "identify",
+                "email",
+                "guilds.members.read"
+            ]
+        }
+
+    ]
+
 });
